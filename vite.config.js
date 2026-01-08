9727bea472aab7aeab30fef29ca5cbae3854b793
LAPTOP-DVLADHIC\Admin <your.name@example.com>
2026-01-08 20:43:38 +0700

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,18 +10,18 @@ export default defineConfig({
       registerType: 'autoUpdate', // Tự động cập nhật khi có nội dung mới
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Tên Ứng Dụng Của Bạn',
-        short_name: 'AppShortName',
-        description: 'Mô tả ngắn về ứng dụng React PWA',
+        name: 'Thanh Nguyen Portfolio',
+        short_name: 'TNPortfolio',
+        description: 'Mô tả ngắn về ứng dụng Thanh Nguyen Portfolio',
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/blogger_icon-icons.com_55480.png', // Bạn phải chuẩn bị icon này trong thư mục public
+            src: 'pwa-192x192.png', // Bạn phải chuẩn bị icon này trong thư mục public
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/blogger_icon-icons.com_55480.png', // Bạn phải chuẩn bị icon này trong thư mục public
+            src: 'pwa-512x512.png', // Bạn phải chuẩn bị icon này trong thư mục public
             sizes: '512x512',
             type: 'image/png'
           }
